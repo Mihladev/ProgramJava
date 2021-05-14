d06c30178a4ea93aab6de72c89f739fa66e17507
Mihladev <84155981+Mihladev@users.noreply.github.com>
2021-05-14 10:22:19 +0200

    Add files via upload

---
+++ ProgramIsFun.java
@@ -0,0 +1,11 @@
+import javax.swing.JOptionPane;
+
+public class ProgramIsFun {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, " Programming is Fun!");
+
+        System.exit(0);
+
+    }
+
+}
